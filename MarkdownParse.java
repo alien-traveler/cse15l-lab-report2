6d9f713d6a5b62a857288206146e44e1dc6f78ca
alien-traveler <jiwenwillluo@gmail.com>
2022-01-28 15:06:48 -0800

    first bug fixed, image done

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -13,10 +13,14 @@ public class MarkdownParse {
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            
+            if(nextOpenBracket == -1 || nextCloseBracket == -1){
+                break;
+            }
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            
+            if(nextOpenBracket == -1 || nextCloseBracket == -1){
+                break;
+            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
